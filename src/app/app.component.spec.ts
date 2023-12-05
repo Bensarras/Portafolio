f8c501aed7ceeceaa0bdf6dab32d9ecbe0ee3497
Benjamin.Sarras <benjamin.sarras@gmail.com>
2023-12-05 02:27:42 -0300

    portafolio commit for deplyment

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,11 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-
+import { HeaderComponent } from './component/header/header.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
+      
     }).compileComponents();
   });
 
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './component/home/home.component';
+import { ContactComponent } from './component/contact/contact.component';
+import { ProjectComponent } from './component/project/project.component';
+import { WorkComponent } from './component/work/work.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'home', component: HomeComponent},
+    { path: 'contact', component: ContactComponent},
+    { path: 'project', component: ProjectComponent},
+    { path: 'work', component: WorkComponent},
+    { path: '**', component: HomeComponent},
+];
